9164835230f37cddeb1d252d973701f55890195c
Soe-Htet-Naung-DP01 <91954268+Soe-Htet-Naung-DP01@users.noreply.github.com>
2021-10-05 19:41:20 +0800

    Journey Start

---
+++ DiscordBot-Project-01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordBot_Project_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
